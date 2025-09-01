d676942b45e676ada2fc7f6792f521906c977584
Sahibzada Ahmad Mujtaba <162528829+mujtabaexe@users.noreply.github.com>
2025-09-02 00:13:34 +0500

    Time: 0 ms (100.00%) | Memory: 13.6 MB (41.80%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,35 +1,10 @@
-
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (target <= nums[i]) {
-                return i; // Insert before or at nums[i]
-            }
+
+        for(int i = 0; i< nums.size(); i++ ){
+            if(target <= nums[i]) return i;
         }
-        return nums.size(); // If target is greater than all elements
+            return nums.size();
     }
 };
-
-
-// class Solution {
-// public:
-//     int searchInsert(vector<int>& nums, int target) {
-        
-//         int index = -1;
-        
-//         if(nums.size() >=1){
-
-//         for(int i = 0; i<= nums.size(); i++ ){
-
-//             index++;
-//             if(target <= nums[i]) return index;
-
-
-//         }
-
-//             index++;
-//             return index;
-
-//         }
-// };
